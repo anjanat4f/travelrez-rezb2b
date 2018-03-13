2b55abcd31a614f17c4b4c049346cf6ceb989eae
Pravin Solanki <iipl.pravins@gmail.com>
2018-03-13 18:43:07 +0530

    fix fields

--- src/Process/GetProductResponse.php
+++ src/Process/GetProductResponse.php
@@ -22,6 +22,8 @@ class GetProductResponse extends Response
 
         $product       = $this->getProductFromData();
 
+        
+
         return [
             "provider_code"               => $product->productBasic->product_id,
             "provider_name"               => "Rezb2b",
@@ -38,8 +40,10 @@ class GetProductResponse extends Response
             "short_description"           => $product->productDescription->brief_description,
             "images"                      => $this->getProductMedia($product),
             "free_sale"                   => $this->getProductFreeSale($product),
-            "booking_fields"              => $this->getProductBookingFields($product),
+            "attributes"                  => $this->getAttributes($product),
             "pickup_locations"            => $this->getPickupLocations($product),
+            "booking_fields"              => $this->getProductBookingFields($product),
+            
         ];
 
     }
@@ -202,6 +206,78 @@ class GetProductResponse extends Response
     public function getPickupLocations($product)
     {
         return array();
+    }
+
+    public function getAttributes($product)
+    {
+
+        $attributes = array();
+
+        if(!empty($product->productUpgrade)) {
+
+            $upgrades = $product->productUpgrade;
+
+            foreach($upgrades as $upgrade) {
+
+                $attributes[] = array(
+                    "attribute_id"        => $upgrade->upgrade_id,
+                    "attribute_name"        => $upgrade->upgrade_name,
+                    "attribute_description" => $upgrade->upgrade_description,
+                    "is_multiple"           => $upgrade->is_multi,
+                    "minimum_required"      => $upgrade->required,
+                    "options"               => $this->getAttributeOptions($upgrade->options)
+                ); 
+
+            }
+
+
+        }
+
+        return $attributes;
+
+
+    }
+
+    public function getAttributeOptions($options = array())
+    {
+
+        $returnOptions = array();
+
+        if(!empty($options)){
+            foreach($options as $option) {
+                $returnOptions[] = array(
+                    "option_id"   => $option->option_id,
+                    "option_name" => $option->option_name,
+                    "sub_options" => isset($option->options) ? $this->getAttributeSubOptions($option->options) : array()
+                );
+
+            }
+        }
+
+        return $returnOptions; 
+
+
+    }
+
+    public function getAttributeSubOptions($options)
+    {
+
+        $returnOptions = array();
+
+        if(!empty($options)){
+            foreach($options as $option) {
+
+                $returnOptions[] = array(
+                    "option_id"   => $option->parent_id."_".$option->option_id,
+                    "option_name" => $option->option_name,
+                );
+
+            }
+        }
+
+        return $returnOptions; 
+
+
     }
 
 }
